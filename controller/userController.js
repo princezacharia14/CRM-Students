eaac0cc9294f00426b927f20b3ef7747257ee077
princezacharia14 <princezacharia14@gmail.com>
2023-04-13 17:12:20 -0400

    Backend server development

---
+++ controller/userController.js
@@ -0,0 +1,32 @@
+const User = require('../model/User');
+
+const register = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, password, confirmPassword} = req.body;
+    
+
+    // Validate the user input
+    if (!firstName || !lastName || !email || !password || !confirmPassword) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    // Check if the email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email is already registered' });
+    }
+
+    // Create a new user
+    const newUser = new User({ firstName, lastName, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    next(err);
+  }
+};
+ 
+module.exports = register
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const authRoute = require('./routes/authRoutes');
+
+dotenv.config();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Mongo DB conncetion
+const database = process.env.MONGOLAB_URI;
+mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
+
+app.set('view engine', 'ejs');
+
+const PORT = process.env.PORT || 4111;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Routes
+app.use('/register', userRoutes);
+app.use('/login', authRoute)
+
+
+// Handle errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong' });
+});
